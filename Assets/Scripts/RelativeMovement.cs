ed8b86f43bc1c67de5107ca4df1549f5d61f3161
Danny Tizon <dtizon@gmail.com>
2016-03-17 16:08:20 -0700

    RelativeMovement script initial

---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RelativeMovement : MonoBehaviour {
+	[SerializeField] private Transform target;
+
+	void Update() {
+		Vector3 movement = Vector3.zero;
+		float horInput = Input.GetAxis("Horizontal");
+		float vertInput = Input.GetAxis("Vertical");
+		if (horInput != 0 || vertInput != 0) {		
+			movement.x = horInput;
+			movement.z = vertInput;
+			Quaternion tmp = target.rotation;
+			target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+			movement = target.TransformDirection(movement);
+			target.rotation = tmp;
+			transform.rotation = Quaternion.LookRotation(movement);
+		}
+	}
+}
